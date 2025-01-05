426807e6fa194d356b002534ee56524ca52f9113
Tushar Goyal <92171383+thetushargoyal@users.noreply.github.com>
2025-01-06 00:35:19 +0530

    added linear regression

--- utils.py
+++ utils.py
@@ -2,3 +2,11 @@ import numpy as np
 
 def eudlidean_distance(x1, x2):
     return np.sqrt(np.sum(x1-x2)**2)
+
+def mean_squared_error(y_true, y_pred):
+    return np.mean((y_true - y_pred) ** 2)
+
+def r2_score(y_true, y_pred):
+    corr_matrix = np.corrcoef(y_true, y_pred)
+    corr = corr_matrix[0, 1]
+    return corr ** 2
