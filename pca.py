d22d1d68a1c25077504b1e9ae8d437820515900d
Tushar Goyal <92171383+thetushargoyal@users.noreply.github.com>
2025-01-23 19:33:50 +0530

    Update pca.py

--- pca.py
+++ pca.py
@@ -1,4 +1,6 @@
 import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import datasets
 
 class PCA:
 
@@ -17,9 +19,34 @@ class PCA:
         eigenvalues = eigenvalues[idxs]
         eigenvectors = eignevectors[idxs]
 
-        self.components = eignevectors[0:self.n_components]
+        self.components = eigenvectors[0:self.n_components]
         
     
     def transform(self, X):
         X = X - self.mean
         return np.dot(X, self.components.T)
+    
+if __name__ == "__main__":
+    data = datasets.load_iris()
+    X = data.data
+    y = data.target
+
+    # Project the data onto the 2 primary principal components
+    pca = PCA(3)
+    pca.fit(X)
+    X_projected = pca.transform(X)
+
+    print("Shape of X:", X.shape)
+    print("Shape of transformed X:", X_projected.shape)
+
+    x1 = X_projected[:, 0]
+    x2 = X_projected[:, 1]
+
+    plt.scatter(
+        x1, x2, c=y, edgecolor="none", alpha=0.8, cmap=plt.cm.get_cmap("viridis", 3)
+    )
+
+    plt.xlabel("Principal Component 1")
+    plt.ylabel("Principal Component 2")
+    plt.colorbar()
+    plt.show()
